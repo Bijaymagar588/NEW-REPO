02339d5918d05a0c7b062551f2c5513e8814e5bc
Bijaymagar588 <bijaymagar588@gmail.com>
2024-07-11 11:02:34 +0545

    First commit

--- src/pages/Service1.jsx
+++ src/pages/Service1.jsx
@@ -1,21 +1,20 @@
 import React from "react";
 import Header from "../components/Homepage/Header";
 import Footer from "../components/Homepage/Footer";
-import Hero from "../components/Services/Hero";
+import Hero from "../components/Services/Hero1";
 import Serviceitems from "../components/Services/Serviceitems";
 
-const Services= () => {
+const Service1 = () => {
   return (
     <main className="text-white">
       <Header />
-      
-        <Hero />
-        <Serviceitems />
- 
-     
-      < Footer/>
+
+      <Hero />
+      <Serviceitems />
+
+      <Footer />
     </main>
   );
 };
 
-export default Services;
+export default Service1;
